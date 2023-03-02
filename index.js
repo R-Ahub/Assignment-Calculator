6e15d909378e84b7b0d519f5f3e756fd1d976da0
MacBook <macbook@MacBooks-MacBook-Pro-2.local>
2023-03-02 09:46:48 +0500

    master

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-console.log("sum")
-
-function sum(){
+console.log("divide")
+function divide() {
     let a=10;
     let b=5;
-    let c=a+b;
+    let c=a/b;
     console.log("result is:",c)
-}
-sum()
+    
+    }
+    divide()
